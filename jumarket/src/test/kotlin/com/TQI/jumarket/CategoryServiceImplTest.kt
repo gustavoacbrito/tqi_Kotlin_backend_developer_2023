9f7fa78d998d242cb15b3e4ce94a44ec69853a86
Gustavo Afonso Chaves de brito <gustavoacbrito@gmail.com>
2023-07-31 18:58:26 -0300

    Improving formatting

--- jumarket/src/test/kotlin/com/TQI/jumarket/CategoryServiceImplTest.kt
+++ jumarket/src/test/kotlin/com/TQI/jumarket/CategoryServiceImplTest.kt
@@ -32,19 +32,12 @@ class CategoryServiceImplTest {
 
     @Test
     fun testListCategoryByName() {
-
         val categoryName = "Books"
         val categories = listOf(
-            Category(1L, "Books"),
-            Category(2L, "Notebooks")
+            Category(1L, "Books"), Category(2L, "Notebooks")
         )
-
         `when`(categoryRepository.findByCategoryNameContainingIgnoreCase(categoryName)).thenReturn(categories)
-
-
         val result = categoryService.listCategoryByName(categoryName)
-
-
         assertEquals(2, result.size)
         assertEquals("Books", result[0].categoryName)
 
@@ -52,52 +45,33 @@ class CategoryServiceImplTest {
 
     @Test
     fun testFindAll() {
-
         val categories = listOf(
-            Category(1L, "Books"),
-            Category(2L, "Notebooks")
+            Category(1L, "Books"), Category(2L, "Notebooks")
         )
-
         `when`(categoryRepository.findAll()).thenReturn(categories)
-
-
         val result = categoryService.findAll()
-
-
         assertEquals(2, result.size)
 
     }
 
     @Test
     fun testFindById() {
-
         val categoryId = 1L
         val category = Category(categoryId, "Books")
-
         `when`(categoryRepository.findById(categoryId)).thenReturn(Optional.of(category))
-
-
         val result = categoryService.findById(categoryId)
-
-
         assertEquals(categoryId, result.id)
         assertEquals("Books", result.categoryName)
-
     }
 
     @Test
     fun testCreate() {
 
         val category = Category(3L, "Books")
-
         `when`(categoryRepository.existsCategoryByCategoryName("Books")).thenReturn(false)
         `when`(categoryRepository.existsById(3L)).thenReturn(false)
         `when`(categoryRepository.save(category)).thenReturn(category)
-
-
         val result = categoryService.create(category)
-
-
         assertEquals(3L, result.id)
         assertEquals("Books", result.categoryName)
 
@@ -107,29 +81,18 @@ class CategoryServiceImplTest {
     fun testCreateCategoryAlreadyExists() {
 
         val category = Category(1L, "Books")
-
         `when`(categoryRepository.existsCategoryByCategoryName("Books")).thenReturn(true)
-
-
         val exception = assertThrows<BusinessRulesException> { categoryService.create(category) }
-
-        // Assertions
         assertEquals(ErrorMessages.CATEGORY_ALREADY_EXISTS, exception.message)
     }
 
     @Test
     fun testUpdate() {
-
         val categoryId = 2L
         val updatedCategory = Category(categoryId, "Electronics")
-
         `when`(categoryRepository.findById(categoryId)).thenReturn(Optional.of(Category(categoryId, "Notebooks")))
         `when`(categoryRepository.save(updatedCategory)).thenReturn(updatedCategory)
-
-
         val result = categoryService.update(categoryId, updatedCategory)
-
-
         assertEquals(categoryId, result.id)
         assertEquals("Electronics", result.categoryName)
 
@@ -137,41 +100,25 @@ class CategoryServiceImplTest {
 
     @Test
     fun testUpdateCategoryNotFound() {
-
         val categoryId = 3L
         val updatedCategory = Category(categoryId, "Toys")
-
         `when`(categoryRepository.findById(categoryId)).thenReturn(Optional.empty())
-
-
         val exception = assertThrows<IllegalArgumentException> { categoryService.update(categoryId, updatedCategory) }
-
-
         assertTrue(exception.message!!.contains(ErrorMessages.CANNOT_UPDATE_CATEGORY))
     }
 
     @Test
     fun testDelete() {
-
         val categoryId = 1L
-
         `when`(categoryRepository.findById(categoryId)).thenReturn(Optional.of(Category(categoryId, "Books")))
-
-
         assertDoesNotThrow { categoryService.delete(categoryId) }
     }
 
     @Test
     fun testDeleteCategoryNotFound() {
-
         val categoryId = 3L
-
         `when`(categoryRepository.findById(categoryId)).thenReturn(Optional.empty())
-
-
         val exception = assertThrows<EntityNotFoundException> { categoryService.delete(categoryId) }
-
-
         assertEquals(ErrorMessages.RECORD_NOT_FOUND, exception.message)
     }
 }
